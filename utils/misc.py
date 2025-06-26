4d5e864385829dfe76433e3066195a6953eee39d
LucMagg <luc.maggiotto@gmail.com>
2025-06-26 23:28:38 +0200

    add utils/misc.py
    -> add group_data_by_hero which groups a full list of lines into groups of 4 lines corresponding to the same hero on Playsome's sheet

---
+++ utils/misc.py
@@ -0,0 +1,21 @@
+def group_data_by_hero(data):
+  if not data:
+    return
+  name_index = data[0].index('Name')
+  groups = []
+  current_group = []
+  current_name = None
+
+  for line in data[1:]:
+    line_name = line[name_index]
+    if current_name is None or line_name == current_name:
+      current_group.append(line)
+      current_name = line_name
+    else:
+      if current_group:
+        groups.append(current_group)
+      current_group = [line]
+      current_name = line_name
+  if current_group:
+    groups.append(current_group)
+  return groups
