05c54e235a5d04f8d64cfcf7d1a8a94d9d195544
LucMagg <luc.maggiotto@gmail.com>
2025-06-26 23:40:57 +0200

    add hero.py
    -> class to process Playsome's data into hero objects

---
+++ classes/hero.py
@@ -0,0 +1,237 @@
+from typing import Dict
+
+
+""" Hero sub-classes """
+class StatsByAscend:
+  def __init__(self):
+    self.A0 = None
+    self.A1 = None
+    self.A2 = None
+    self.A3 = None
+
+  def from_dict(cls, data: Dict):
+    return cls(
+      A0 = data.get('A0', None),
+      A1 = data.get('A0', None),
+      A2 = data.get('A0', None),
+      A3 = data.get('A0', None)
+    )
+
+  def to_dict(self) -> Dict:
+    return {
+      'A0': self.A0,
+      'A1': self.A1,
+      'A2': self.A2,
+      'A3': self.A3
+    }
+
+class Leader:
+  def __init__(self):
+    self.attack = None
+    self.defense = None
+    self.color = None
+    self.species = None
+    self.talent = None
+    self.extra = None
+
+  def from_dict(cls, data: Dict):
+    return cls(
+      attack = data.get('attack', None),
+      defense = data.get('defense', None),
+      color = data.get('color', None),
+      species = data.get('species', None),
+      talent = data.get('talent', None),
+      extra = data.get('extra', None)
+    )
+
+  def to_dict(self) -> Dict:
+    return {
+      'attack': self.attack,
+      'defense': self.defense,
+      'color': self.color,
+      'species': self.species,
+      'talent': self.talent,
+      'extra': self.extra
+    }
+  
+class Talent:
+  def __init__(self):
+    self.base = []
+    self.A1 = None
+    self.A2 = None
+    self.A3 = None
+    self.merge = []
+  
+  def from_dict(cls, data: Dict):
+    return cls(
+      base = data.get('attack', []),
+      A1 = data.get('A1', ''),
+      A2 = data.get('A2', ''),
+      A3 = data.get('A3', ''),
+      merge = data.get('merge', [])
+    )
+
+  def to_dict(self) -> Dict:
+    return {
+      'base': self.base,
+      'A1': self.A1,
+      'A2': self.A2,
+      'A3': self.A3,
+      'merge': self.merge
+    }
+  
+class Display():
+  def __init__(self):
+    pass
+
+""" Hero class """
+class Hero:
+  def __init__(self, logger, special_heroes, events, elements_templates):
+    self.logger = logger
+    self.special_heroes = special_heroes
+    self.events = events
+    self.elements_templates = elements_templates
+
+    self.name = None
+    self.heroclass = None
+    self.stars = None
+    self.levelmax = StatsByAscend()
+    self.AI = None
+    self.AI_speed = None
+    self.attack = StatsByAscend()
+    self.health = StatsByAscend()
+    self.color = None
+    self.species = None
+    self.talents = Talent()
+    self.gear = StatsByAscend()
+    self.exclusivity = None
+    self.leaderA = Leader()
+    self.leaderB = Leader()
+    self.display = Display()
+
+  def to_dict(self) -> Dict:
+    return {
+      'name': self.name,
+      'heroclass': self.heroclass,
+      'stars': self.stars,
+      'levelmax': self.levelmax.to_dict(),
+      'AI': self.AI,
+      'AI_speed': self.AI_speed,
+      'attack': self.attack.to_dict(),
+      'health': self.health.to_dict(),
+      'color': self.color,
+      'species': self.species,
+      'talent' : self.talents.to_dict(),
+      'gear': self.gear.to_dict(),
+      'exclusivity': self.exclusivity,
+      'leaderA': self.leaderA.to_dict(),
+      'leaderB': self.leaderB.to_dict()
+    }
+
+  def create_hero(self, data, header):
+    """ Create Hero object
+      Args:
+        data: list of lines (group) with same hero name
+        header: Playsome's sheet header row
+      Returns:
+        hero object (self)
+    """
+    self.name = self._recolor_hero(data[0][header.index('Name')])
+    self.heroclass = data[0][header.index('Class')]
+    self.stars = data[0][header.index('Stars')]
+    self.AI = str.capitalize(data[0][header.index('AI')][:-3])
+    if self.AI == 'Support':
+      self.AI = 'Supporter'
+    self.AI_speed = str(data[0][header.index('Speed')])
+    self.color = data[0][header.index('Color')]
+    self.species = data[0][header.index('Species')]
+    self.exclusivity = self.events[data[0][header.index('Exclusivity')]] if data[0][header.index('Exclusivity')] != '' else ''
+    for line in data:
+      ascend = f'A{line[header.index('Ascension')]}'
+      setattr(self.levelmax, ascend, line[header.index('LevelCap')])
+      setattr(self.attack, ascend, line[header.index('Attack Cap')])
+      setattr(self.health, ascend, line[header.index('Health Cap')])
+      setattr(self.gear, ascend, self._get_gear(line, header))
+    self._get_talents(data=data, header=header)
+    self._get_leader(line=line[:-1], header=header)
+    return self
+  
+  """ class Hero private methods for sheets data parsing """
+  def _get_gear(self, line, header):
+    to_return = []
+    for i, case in enumerate(header):
+      if case == 'Gear':
+        to_return.append(line[i])
+    return to_return
+  
+  def _get_talents(self, data, header):
+    talents_start = header.index('Talents')
+    talents_end = self._get_last_index(header, 'Talents')
+    for i in range(talents_start, talents_end):
+      if data[0][i] == '':
+        ascend_talents_start = i
+        break
+      self.talents.base.append(data[0][i].replace(' Of ',' of '))
+    self.talents.A1 = data[1][ascend_talents_start].replace(' Of ',' of ')
+    self.talents.A2 = data[2][ascend_talents_start + 1].replace(' Of ',' of ')
+    self.talents.A3 = data[3][ascend_talents_start + 2].replace(' Of ',' of ')
+
+    merge_talents_start = header.index('Mastery Talents')
+    self.talents.merge = [data[0][mt].replace(' Of ',' of ') for mt in range(merge_talents_start, merge_talents_start + 3) if data[0][merge_talents_start] != '']
+
+  def _get_last_index(self, list, element):
+    for i in range(len(list) - 1, -1, -1):
+      if list[i] == element:
+        return i
+    self.logger.error(f'{element} not in header, please check data and run again')
+
+  def _get_leader(self, line, header) -> Leader:
+    match line[header.index('LeaderBuffA')]:
+      case 'ExtraTime':
+        self.leaderA.talent = self._add_spaces_to_talent(line[header.index('LeaderBuffA')])
+        self.leaderA.color = line[header.index('Req A')]
+      case 'LeaderAttack':
+        self.leaderA.color = line[header.index('Req A')]
+        self.leaderA.attack = line[header.index('LeaderAtkMultiplier')]
+        self.leaderB.species = line[header.index('Req B')]
+        self.leaderB.defense = line[header.index('LeaderDefMultiplier')]
+      case _:
+        self.leaderA.color = line[header.index('Req A')]
+        match line[header.index('LeaderRequirement Operator')]:
+          case 'AND':
+            self.leaderA.species = line[header.index('Req A2')]
+          case 'OR':
+            self.leaderA.extra = self.events[line[header.index('Req A2')]]
+        self.leaderA.attack = line[header.index('LeaderAtkMultiplier')]
+        self.leaderA.defense = line[header.index('LeaderDefMultiplier')]
+        self.leaderB.species = line[header.index('Req B')]
+        if line[header.index('LeaderBuffB')].startswith('Bard'):
+          self.leaderB.talent = self._bard_talent_special_case(line[header.index('LeaderBuffB')])
+        else:
+          self.leaderB.talent = self._add_spaces_to_talent(line[header.index('LeaderBuffB')])
+
+  def _bard_talent_special_case(self, talent):
+    try:
+      return self.special_heroes['Talents'][talent]
+    except:
+      self.logger.error(f'{talent} not in Talents\' section of special_heroes.yml, please add it and run again')
+
+  def _add_spaces_to_talent(self, talent):
+    if not talent:
+      return talent
+    to_return = talent[0]
+    for char in talent[1:]:
+      if char.isupper():
+        to_return += ' ' + char
+      else:
+        to_return += char
+    return to_return
+  
+  def _recolor_hero(self, name: str):
+    try:
+      if name[-1] == 'S':
+        return self.special_heroes['Heroes'][name]
+      else:
+        return name
+    except:
+      self.logger.error(f'{name} not in Heroes\' section of special_heroes.yml, please add it and run again')
