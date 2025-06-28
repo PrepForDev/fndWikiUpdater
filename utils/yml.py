c62fecb28f51b7baf71d1c1a0b751e368f256ac1
LucMagg <luc.maggiotto@gmail.com>
2025-06-28 18:41:23 +0200

    update yml.py
    -> delete extension to handle glob files

--- utils/yml.py
+++ utils/yml.py
@@ -8,11 +8,11 @@ class Yml:
   def load(self, file, data_dir=None):
     try:
       if data_dir is not None:
-        full_file_path = os.path.join(data_dir, f'{file}.yml')
+        full_file_path = os.path.join(data_dir, file)
       else:
         full_file_path = file
       with open(full_file_path, 'r', encoding='utf-8') as f:
         return yaml.load(f, Loader=yaml.SafeLoader)
     except FileNotFoundError as e:
-      self.logger.error(f'File {file}.yml not found in /{data_dir}')
+      self.logger.error(f'File {file} not found in /{data_dir}')
       return False
