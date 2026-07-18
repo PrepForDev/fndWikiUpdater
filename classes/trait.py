930f8a25a4185046bf9da1bdedb759cb58055ccf
LucMagg <luc.maggiotto@gmail.com>
2026-07-18 05:33:45 +0200

    update trait class
    -> update _match_images_with_traits

--- classes/trait.py
+++ classes/trait.py
@@ -54,6 +54,7 @@ class Trait:
     """
     self.name = data[header.index('Name')]
     self.special_art_id = data[header.index('Special_Art_ID')] if data[0][header.index('Special_Art_ID')] != '' else None
+    self.playsome_art_id = data[header.index('Playsome_Art_ID')] if data[0][header.index('Playsome_Art_ID')] != '' else None
     self.formatted_name = data[header.index('Formatted')]
     self.type = data[header.index('type')]
     self.sub_type = data[header.index('sub_type')]
@@ -63,18 +64,19 @@ class Trait:
 def match_images_with_traits(ctx, images: List[Dict], attribute: str):
   """ Match image list with extracted traits objects """
   for image in images:
-    cleaned_image_name = image.get('name').split('.png')[0][5:]
-    # match with trait name
-    found_trait = next((trait for trait in ctx.traits if cleaned_image_name.lower() == trait.name.replace(' ','').lower()), None)
-    if found_trait:
-      setattr(found_trait.file, attribute, image)
-      ctx.logger.debug(f'  Trait pic : {image.get('name')} | found for {found_trait.name}')
+    if _match_trait(ctx, image, 'playsome_art_id', attribute):
       continue
-    # match with trait special_art_id
-    found_trait = next((trait for trait in ctx.traits if cleaned_image_name.lower() == trait.special_art_id.lower()), None)
-    if found_trait:
-      setattr(found_trait.file, attribute, image)
-      ctx.logger.debug(f'  Trait pic : {image.get('name')} | found for {found_trait.name}')
+    if _match_trait(ctx, image, 'special_art_id', attribute):
       continue
-    # no match found
+    if _match_trait(ctx, image, 'name', attribute):
+      continue
     ctx.logger.info(f'  Trait pic : {image.get('name')} didn\'t match any trait')
+
+def _match_trait(ctx, image, attr_to_compare: str, file_attribute: str) -> bool:
+  cleaned_image_name = image.get('name').split('.png')[0][5:].lower()
+  found_trait = next((trait for trait in ctx.traits if getattr(trait, attr_to_compare) and cleaned_image_name == getattr(trait, attr_to_compare).replace(' ','').replace(':','').replace('[','').replace('(','').replace(']','').replace(')','').replace('!','').lower()), None)
+  if found_trait:
+    setattr(found_trait.file, file_attribute, image)
+    ctx.logger.debug(f'  Trait pic : {image.get('name')} | found for {found_trait.name} ({attr_to_compare})')
+    return True
+  return False
